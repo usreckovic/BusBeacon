cb5a7673d45f9a220122c22827bb82b2e48e7122
Uros Sreckovic <urke23295@gmail.com>
2026-06-01 23:25:45 +0200

    Inital commit

---
+++ src/main/java/rs/ac/busbeacon/BusbeaconApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.busbeacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusbeaconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusbeaconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/busbeacon/BusbeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.busbeacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusbeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
